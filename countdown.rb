d940923bc06bf23c31cce53868bf42839424bc35
Mike Shippy <mikeship04@gmail.com>
2022-07-21 17:16:28 -0700

    commit tree b6827fe3ca6ba11e35eed2172da63616b5bc2efc

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+x=10
+def countdown(x)
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(x)
+    while x > 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep(5)
+    end
+    "HAPPY NEW YEAR!"
+
 end
